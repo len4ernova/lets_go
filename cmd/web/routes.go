ccb628a201ce169f5078693d474fc702029042f3
chernish <len4ernova@yandex.ru>
2025-12-18 19:10:04 +0300

    Finished 7

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
