8930ab9dac72d1db515ec5979616dad8118dbde1
elena4ernova <eachernova@iifmail.ru>
2025-12-18 16:14:21 +0300

    встраивание validator

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,10 +5,24 @@ import (
 	"fmt"
 	"github/len4ernova/lets_go/internal/models"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	// "html/template"
 	"net/http"
 )
+
+// Define a snippetCreateForm struct to represent the form data and validation
+// errors for the form fields. Note that all the struct fields are deliberately
+// exported (i.e. start with a capital letter). This is because struct fields
+// must be exported in order to be read by the html/template package when
+// rendering the template.
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// panic("oops! something went wrong") // Deliberate panic
@@ -56,27 +70,50 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	// Используйте метод r.PostForm.Get() для извлечения заголовка и содержимого
-	// из карты r.PostForm.
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	// Метод r.PostForm.Get() всегда возвращает данные формы в виде *строки*.
-	// Однако мы ожидаем, что значение expires будет числом, и хотим
-	//  представить его в нашем коде Go как целое число. Поэтому нам нужно
-	// вручную преобразовать данные формы в целое число с помощью
-	// strconv.Atoi(), и мы отправим ответ с кодом 400 «Неверный запрос»,
-	// если преобразование не удастся
+	// Get the expires value from the form as normal.
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	id, err := app.snippets.Insert(title, content, expires)
+	// Create an instance of the snippetCreateForm struct containing the values
+	// from the form and an empty map for any validation errors.
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+
+	// Update the validation checks so that they operate on the snippetCreateForm
+	// instance.
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+	// Check that the Content value isn't blank.
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+	// Check the expires value matches one of the permitted values (1, 7 or
+	// 365).
+	if expires != 1 && expires != 7 && expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+	// If there are any errors, dump them in a plain text HTTP response and
+	// return from the handler.
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-	// Redirect the user to the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
@@ -84,5 +121,12 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 // against *application.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+	// Initialize a new snippetCreateForm instance and pass it to the template.
+	// Notice how this is also a great opportunity to set any default or
+	// 'initial' values for the form --- here we set the initial value for the
+	// snippet expiry to 365 days.
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
