989fcb41ad10735cdb9d15483c795b4b396ceb9d
elena4ernova <eachernova@iifmail.ru>
2025-12-30 09:19:06 +0300

    add interface in app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,8 +28,8 @@ import (
 // make the SnippetModel object available to our handlers.
 type application struct {
 	logger         *slog.Logger
-	snippets       *models.SnippetModel
-	users          *models.UserModel
+	snippets       models.SnippetModelInterface // Use our new interface type.
+	users          models.UserModelInterface    // Use our new interface type.
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
